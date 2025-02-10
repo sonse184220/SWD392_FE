abb86308d35b32a8016dd07c4902e273c942a746
sonse184220 <sonmtse184220@fpt.edu.vn>
2025-02-10 21:38:16 +0700

    implement admin template

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+// module.exports = {
+//   plugins: [
+//     require('@tailwindcss/postcss')(),
+//     require('autoprefixer'),
+//   ],
+// };
+// module.exports = {
+//   plugins: {
+//     tailwindcss: require("tailwindcss"),
+//     autoprefixer: require("autoprefixer"),
+//   },
+// };
+
